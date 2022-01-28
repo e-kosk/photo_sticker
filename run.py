7edfa78136488f0775df01f36a9c6018d435e78d
Eryk Kosk <eryk.kosk@wp.pl>
2022-01-28 22:38:04 +0100

    change default color collation class

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     parser.add_argument('main_photo', type=str, help='path of the main photo')
     parser.add_argument('--output', type=str, help='output path of processed image', default='processed.jpg')
     parser.add_argument('--color-picker', type=str, help='color picker class', default='resize')
-    parser.add_argument('--color-collation', type=str, help='color comparing class', default='euclidean')
+    parser.add_argument('--color-collation', type=str, help='color comparing class', default='deltae')
     args = parser.parse_args()
 
     image = ImageSticker(args.main_photo, args.photos_dir, args.color_picker, args.color_collation)
